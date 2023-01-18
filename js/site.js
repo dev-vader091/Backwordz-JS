1575258fb4d29a0b9782fbf65f6deac77cf45372
dev-vader091 <86165493+dev-vader091@users.noreply.github.com>
2023-01-18 14:05:06 -0600

    added final javascript functionality

--- js/site.js
+++ js/site.js
@@ -1,11 +1,35 @@
-function displayMessage() {
-  let msg = document.getElementById('message').value;
+// entry point: controller function
+function getValues() {
+    let inputString = document.getElementById('userString').value;
 
-  Swal.fire(
-    {
-      backdrop: false,
-      title: 'Backwordz',
-      text: msg
+    
+    let reversedString = reverseAString(inputString);
+    
+    displayString(reversedString);
+}
+
+// logic function
+// reverse a string 
+function reverseAString(userString) {
+    // reverse the string
+    // let revesedStringArray = [];
+    let revString = "";
+    // userString = userString.split('');
+    for(i = userString.length - 1; i >= 0; i--) {
+        let currentLetter = userString[i];
+
+        revString += currentLetter;
+
     }
-  );
+
+
+
+
+  return revString ;
+}
+
+// view function
+function displayString(reversedString) {
+    document.getElementById('results').textContent = reversedString;
+    document.getElementById('alert').classList.remove('invisible');
 }
